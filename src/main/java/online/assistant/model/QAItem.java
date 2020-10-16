042ec8142d5d1adc438a269f74fcf68fc87f2425
hetikk <h.kastuev99@inbox.ru>
2020-10-16 11:56:57 +0300

    новвые модели данных

---
+++ src/main/java/online/assistant/model/QAItem.java
@@ -0,0 +1,58 @@
+package online.assistant.model;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class QAItem implements Serializable, Comparable<QAItem> {
+
+    private int id;
+    private String text;
+
+    public QAItem() {
+    }
+
+    public QAItem(int id, String text) {
+        this.id = id;
+        this.text = text;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    @Override
+    public int compareTo(QAItem o) {
+        return text.compareTo(o.text);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        QAItem qaItem = (QAItem) o;
+        return id == qaItem.id &&
+                text.equals(qaItem.text);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, text);
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s (id=%d)", text, id);
+    }
+}
---
+++ src/main/java/online/assistant/model/QuestionAnswers.java
@@ -0,0 +1,94 @@
+package online.assistant.model;
+
+import com.google.gson.annotations.SerializedName;
+
+import java.io.Serializable;
+import java.util.Collections;
+import java.util.List;
+
+public class QuestionAnswers implements Serializable, Comparable<QuestionAnswers> {
+
+    private QAItem question;
+    private List<QAItem> answers;
+    @SerializedName("right_answer")
+    private QAItem rightAnswer;
+    @SerializedName("multiple_choice")
+    private boolean multipleChoice;
+
+    public QuestionAnswers() {
+    }
+
+    public QuestionAnswers(QAItem question, List<QAItem> answers, QAItem rightAnswer, boolean multipleChoice) {
+        this.question = question;
+        setAnswers(answers);
+        this.rightAnswer = rightAnswer;
+        this.multipleChoice = multipleChoice;
+    }
+
+    public QAItem getQuestion() {
+        return question;
+    }
+
+    public void setQuestion(QAItem question) {
+        this.question = question;
+    }
+
+    public List<QAItem> getAnswers() {
+        return answers;
+    }
+
+    public void setAnswers(List<QAItem> answers) {
+        Collections.sort(answers);
+        this.answers = answers;
+    }
+
+    public QAItem getRightAnswer() {
+        return rightAnswer;
+    }
+
+    public void setRightAnswer(QAItem rightAnswer) {
+        this.rightAnswer = rightAnswer;
+    }
+
+    public boolean isMultipleChoice() {
+        return multipleChoice;
+    }
+
+    public void setMultipleChoice(boolean multipleChoice) {
+        this.multipleChoice = multipleChoice;
+    }
+
+    @Override
+    public int compareTo(QuestionAnswers o) {
+        return question.compareTo(o.question);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        QuestionAnswers that = (QuestionAnswers) o;
+        return question.equals(that.question);
+    }
+
+    @Override
+    public int hashCode() {
+        return question.hashCode();
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder builder = new StringBuilder();
+
+        builder.append(String.format("Вопрос: %s (id=%d, multiple_choice=%b)\n",
+                question.getText(), question.getId(), multipleChoice));
+
+        for (QAItem answer : answers) {
+            builder.append("  · ").append(answer.toString()).append("\n");
+        }
+
+        builder.append("Правильный ответ: ").append(rightAnswer == null ? null : rightAnswer.toString());
+
+        return builder.toString();
+    }
+}
